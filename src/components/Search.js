9e6519c2393f4337ef2e42eca1c9c8bd05dc305a
PHILIP COSTACHE <nogobaduk@gmail.com>
2020-08-24 14:52:11 -0400

    rendering result list

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,10 +3,11 @@ import axios from 'axios';
 
 const Search = () => {
    const [ term, setTerm ] = useState('');
+   const [ results, setResults ] = useState([]);
 
    useEffect(() => {
-      (async () => {
-         const { data } = await axios.get(`https://en.wikipedia.org/w/api.php`, {
+      const search = async () => {
+         const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
             params: {
                action: 'query',
                list: 'search',
@@ -14,10 +15,23 @@ const Search = () => {
                format: 'json',
                srsearch: term
             }
-         })
-      })();
-
+         });
+         setResults(data.query.search);
+      }
+      term && search();
    }, [term]);
+   const renderedResults = results.map(result => {
+      return (
+         <div key={result.pageid} className='item'>
+            <div className='content'>
+               <div className='header'>
+                  {result.title}
+               </div>
+               {result.snippet}
+            </div>
+         </div>
+      )
+   }); 
    return (
       <div>
          <div className='ui form'>
@@ -30,6 +44,9 @@ const Search = () => {
                >  
                </input>
             </div>
+         </div>
+         <div className='ui celled list'>
+            {renderedResults}
          </div>
       </div>
    );
