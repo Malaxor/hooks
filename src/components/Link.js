1e2214b15169f77b0425f36a2c1a331f70c9cd9c
PHILIP COSTACHE <nogobaduk@gmail.com>
2020-08-26 16:17:36 -0400

    Nav completed

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Link = ({ href, className, children }) => {
    const onClick = event => {
+      if(event.metaKey || event.ctrlKey) {
+         return;
+      }
       event.preventDefault(); // prevent browser reload
       window.history.pushState({}, "", href); // update URL
+
+      const navEvent = new PopStateEvent('popstate'); // communicate to Route component that the URL has changed
+      window.dispatchEvent(navEvent);
    }
    return (
       <a onClick={onClick} className={className} href={href}>
--- src/components/Route.js
+++ src/components/Route.js
@@ -1,5 +1,17 @@
+import { useEffect, useState } from 'react';
+
 const Route = ({ path, children }) => {
-   if(window.location.pathname === path) {
+   const [ currentPath, setCurrentPath ] = useState(window.location.pathname);
+
+   useEffect(() => {
+      const onLocationChange = () => {
+         setCurrentPath(window.location.pathname);
+      }
+      window.addEventListener('popstate', onLocationChange);
+      return () => window.removeEventListener('popstate');
+   }, []);
+
+   if(currentPath === path) {
       return children;
    } 
    return null;
